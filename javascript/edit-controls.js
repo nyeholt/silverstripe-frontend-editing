8f4e6fc2f1dcabbb74cb1ed84e072a298009a59d
nyeholt <nyeholt@gmail.com>
2010-09-01 20:42:06 +1000

    Updated to remove duplication of query string vars

--- javascript/edit-controls.js
+++ javascript/edit-controls.js
@@ -1,15 +1,27 @@
 (function($){
+	function cleanUrl() {
+		var cur = location.href;
+		cur = cur.replace(/&stage=[a-zA-Z]+/g, '');
+		cur = cur.replace(/\?stage=[a-zA-Z]+/g, '');
+		cur = cur.replace(/&startEditing=true/g, '');
+		return cur;
+	}
+
 	$().ready(function () {
 		$('#FE_ViewPublished').click(function () {
-			var sep = location.href.indexOf('?') > 0 ? '&' : '?';
-			location.href = location.href + sep + 'stage=Live';
+			var cur = cleanUrl();
+			var sep = cur.indexOf('?') > 0 ? '&' : '?';
+			location.href = cur + sep + 'stage=Live';
 		})
 	});
 
 	$().ready(function () {
 		$('#FE_EditDraft').click(function () {
-			var sep = location.href.indexOf('?') > 0 ? '&' : '?';
-			location.href = location.href + sep + 'stage=Stage&startEditing=true';
+			var cur = cleanUrl();
+			var sep = cur.indexOf('?') > 0 ? '&' : '?';
+			location.href = cur + sep + 'stage=Stage&startEditing=true';
 		})
 	});
+
+
 })(jQuery);
